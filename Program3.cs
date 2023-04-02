28a510d1ecf8e2a9facb6866eff9b6a37fe2546d
Miraghaa <miraghaea@code.edu.az>
2023-04-02 18:39:33 +0400

    program3.cs

---
+++ Program3.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+int[] numbers = { 1, 5, 105, 25, 93, 100 };
+int i = 0;
+int boyuk = numbers[0];
+while (i < numbers.Length) 
+{
+    if(numbers[i] >boyuk  )
+    {
+        boyuk = numbers[i];
+    }
+    i++;
+
+}
+
+Console.WriteLine(boyuk);
