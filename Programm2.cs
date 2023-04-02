99dd9394e77de289716c25ee8d3904082e6f4945
Miraghaa <129064566+Miraghaa@users.noreply.github.com>
2023-04-02 23:10:01 +0400

    Update Programm2.cs

--- Programm2.cs
+++ Programm2.cs
@@ -1,12 +1,10 @@
 ﻿int[] numbers = { 15, -20, 5, 7, -5, -6, -7, 87, 90, 64 };
 int i = 0;
-int j = 1;
 int musbet = 0;
 int menfi = 0;
 while (i < numbers.Length)
 
 {
-    numbers[i] = numbers[i] / j;
     if (numbers[i] > 0)
     {
         musbet++;
